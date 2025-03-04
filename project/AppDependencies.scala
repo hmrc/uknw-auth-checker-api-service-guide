a88697495b23e509764e7086eb546ee2a5afc6bb
Dan Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2025-03-04 13:19:26 +0000

    NP-Upgrade: Updated Scala, Sbt, Plugin & Dependency versions (#62)
    * NP-Upgrade: Updated Scala, Sbt, Plugin & Dependency versions
    
    * NP-Upgrade: Updated Scala, Sbt, Plugin & Dependency versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import play.sbt.PlayImport.*
 import sbt.*
 
 object AppDependencies {
-  lazy val bootStrapPlayVersion = "9.9.0"
+  lazy val bootStrapPlayVersion = "9.11.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     ws,
