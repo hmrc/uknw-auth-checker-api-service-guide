87d60a966cfcf0a3fc8e4b47f306b1526a19499f
Dan Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2025-07-23 11:49:22 +0100

    NP-386: Upgraded bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import play.sbt.PlayImport.*
 import sbt.*
 
 object AppDependencies {
-  lazy val bootStrapPlayVersion = "9.15.0"
+  lazy val bootStrapPlayVersion = "9.18.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     ws,
